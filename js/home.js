8cee52dff03fccb88cc43b1a73ab746726b0fd39
DucGiangBui <2051060481@e.tlu.edu.vn>
2022-03-30 10:19:14 +0700

    update 30/03

--- js/home.js
+++ js/home.js
@@ -1,53 +1,17 @@
-<script src="http://code.jquery.com/jquery-latest.js"></script>;
-$(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) $(".lentop").fadeIn();
-    else $(".lentop").fadeOut();
-  });
-  $(".lentop").click(function () {
-    $("body,html").animate({ scrollTop: 0 }, "slow");
-  });
-});
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+function openComment(evt, cityName) {
+  var i, tabcontent, tablinks;
 
-var appendNumber = 4;
-var prependNumber = 1;
-document
-  .querySelector(".prepend-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-    ]);
-  });
-  
-var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabcursor: true,
-  centeredslides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideshadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
